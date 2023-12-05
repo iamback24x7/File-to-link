195e565bbda8185e546780e3ee9e964c26978613
iamback24x7 <107175697+iamback24x7@users.noreply.github.com>
2023-12-05 09:53:13 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -37,24 +37,27 @@ async def render_page(id, secure_hash):
     current_url = f'{Var.URL}/{str(id)}/{file_data.file_name}?hash={secure_hash}'
     html_code = f'''
    <p>
+     
+          <center><b>This link expires after 24 hours.</b></center>
+         
+          <b>
+            <a href="{current_url}" type="button" id="demo" class="btn btn-primary btn-block mb-4">
+              Download Now
+            </a>
+          </b>
+      
     <center><h5>Click on 👇 button to watch/download in your favorite player</h5></center>
-    <center>
-        <button style="font-size: 20px; background-color: skyblue; border-radius: 10px;" onclick="window.location.href = 'intent:{current_url}#Intent;package=com.mxtech.videoplayer.ad;S.title={file_data.file_name};end'">MX player</button> &nbsp
-        <button style="font-size: 20px; background-color: orange; border-radius: 10px;" onclick="window.location.href = 'vlc://{current_url}'">VLC player</button> &nbsp <br>
-        <p>&nbsp</p>
-        <button style="font-size: 20px; background-color: red; border-radius: 10px;" onclick="window.location.href = 'playit://playerv2/video?url={current_url}&amp;title={file_data.file_name}'">Playit player</button> &nbsp <br>
-        <p>&nbsp</p>
-        <button style="font-size: 20px; background-color: yellow; border-radius: 10px;" onclick="window.location.href = '{current_url}'">Save in your gallery</button> &nbsp
-    </center>
+
+
+
+      <center>
+       <a href="vlc://{current_url}"  ><button>👀 VLC Player</button></a>
+       <a href="playit://playerv2/video?url={current_url}&amp;title={file_data.file_name}" class="playit" > <button onclick="window.location.href=''">👀 Playit app</button></a>
+       <a href="intent:{current_url}#Intent;package=com.mxtech.videoplayer.ad;S.title={file_data.file_name};end" class="mxplayer" > <button>👀 MX Player</button></a><br>
+      </center>
 </p>
 </p>
-<center>
-    <h2>
-        <a href="https://telegram.dog/+kc6bYRCsWdlhOTI1">
-            <img src="https://graph.org/file/b57cdba982191a25db535.jpg" alt="Rkbotz" width="150" height="75">
-        </a>
-    </h2>
-</center>
+
 
 '''
 
