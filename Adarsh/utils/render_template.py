cff04a651068f902bd4677fb3013e0700cc222db
iamback24x7 <107175697+iamback24x7@users.noreply.github.com>
2023-12-05 10:12:00 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -34,7 +34,7 @@ async def render_page(id, secure_hash):
                     heading = 'Download {}'.format(file_data.file_name)
                     file_size = humanbytes(int(u.headers.get('Content-Length')))
                     html = (await r.read()) % (heading, file_data.file_name, src, file_size)
-    current_url = f'{Var.URL}/{str(id)}/{file_data.file_name}?hash={secure_hash}'
+    current_url = f"{Var.URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
     html_code = f'''
    <p>
      
